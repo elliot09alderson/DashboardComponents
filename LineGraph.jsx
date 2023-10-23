73c6aeeb7c58b4429c7257b85a9c268448998baf
elliot09alderson <50716820+elliot09alderson@users.noreply.github.com>
2023-10-23 23:12:29 +0530

    Create LineGraph.jsx

---
+++ LineGraph.jsx
@@ -0,0 +1,92 @@
+import React from "react";
+import { Line } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+// import faker from "faker";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+export const options = {
+  responsive: true,
+  interaction: {
+    mode: "index",
+    intersect: false,
+  },
+  stacked: false,
+  plugins: {
+    title: {
+      display: true,
+      text: "Chart.js Line Chart - Multi Axis",
+    },
+  },
+  scales: {
+    y: {
+      type: "linear",
+      display: true,
+      position: "left",
+    },
+    y1: {
+      type: "linear",
+      display: true,
+      position: "right",
+      grid: {
+        drawOnChartArea: false,
+      },
+    },
+  },
+};
+
+const labels = ["January", "February", "March", "April", "May", "June", "July"];
+
+export const data = {
+  labels,
+  datasets: [
+    {
+      label: "Dataset 1",
+      data: [2000, 32, 903, 124, 223, 1000, 323],
+      borderColor: "rgb(255, 99, 132)",
+      backgroundColor: "rgba(255, 99, 132, 0.5)",
+      yAxisID: "y",
+    },
+    {
+      label: "Dataset 2",
+      data: [1212, 232, 990, 684, 231, 100, 1223],
+      borderColor: "rgb(53, 162, 235)",
+      backgroundColor: "rgba(53, 162, 235, 0.5)",
+      yAxisID: "y1",
+    },
+  ],
+};
+
+function MultiLineChart() {
+  return (
+    <div className="LineGraph ">
+      <Line options={options} width={"1200px"} height={"600px"} data={data} />;
+    </div>
+  );
+}
+const App = () => {
+  return (
+    <div className="w-full">
+      <MultiLineChart />
+    </div>
+  );
+};
+
+export default App;
